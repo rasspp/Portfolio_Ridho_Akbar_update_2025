5c1d12f7b67beb0df7a1238080fdcf886282eb4c
rasspp <ridho8977@gmail.com>
2025-02-26 13:46:23 +0000

    update: compoent to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact"
-import Navbar from "./components/navbar/Navbar";
+import Profile from "./pages/Profil";
 
 
 function App() {
@@ -13,9 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Profile />} />
       </Routes>
     </Router>
   );
